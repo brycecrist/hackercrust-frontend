0a6e7c6da107aa9627a4e65f918fb70177a08885
Bryce Crist <tech.bcrist@gmail.com>
2023-11-25 22:23:28 -0700

    Adds in-memory support for page loading, and style changes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import {Pagination} from "./components/pagination";
 import {CircularProgress} from "@mui/material";
 import packageJson from "../package.json"
 import {useLocation} from "react-router-dom";
+import * as data from "./utils/data";
+import {cachedStories} from "./utils/data";
 
 const App = () => {
   const [storyIds, setStoryIds] = useState([])
@@ -30,18 +32,59 @@ const App = () => {
     (async () => {
       setLoading(true)
 
+      console.log(storiesAreInCache())
+
+      if (storiesAreInCache()) {
+        console.log("GOT IT BYE!")
+        setLoading(false)
+        return
+      }
+
       const fetchStoryIds = async () => await getTopStories()
       const fetchStories = async () => await getNumberOfStories(storyFilters)
 
       const storyIds = await fetchStoryIds()
       if (storyIds)
         setStoryIds(storyIds)
-      if (storyFilters)
+      if (storyFilters) {
+        console.log("Fetching Stories")
         setStories(await fetchStories())
+        console.log(stories)
+      }
 
       setLoading(false)
     })()
   }, [storyFilters])
+
+  useEffect(() => {
+    if (!cachedStories.hasOwnProperty(storyFilters.page)) {
+      console.log("Hola")
+      if (stories.length > 0) {
+        const entry = {
+          page: storyFilters.page,
+          stories: stories
+        }
+
+        cachedStories.viewed.push(entry)
+        console.log(cachedStories.viewed)
+      }
+    }
+  }, [stories])
+
+  const storiesAreInCache = () => {
+    let foundPage = false
+    cachedStories.viewed.forEach(i => {
+      console.log("I!")
+      console.log(i)
+      if (i.page === storyFilters.page) {
+        console.log("FOUND IT!")
+        setStories(i.stories)
+        foundPage = true
+      }
+    })
+
+    return foundPage
+  }
 
   // If no stories due to server being down, give empty list/*
   const storiesToDisplay = stories ? stories.map(
